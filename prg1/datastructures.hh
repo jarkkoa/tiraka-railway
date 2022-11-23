a87dd620e62b785899f4fb3f92f7ed2b8e6c6756
Jarkko Ahvenniemi <Jarge98@gmail.com>
2022-11-23 19:58:44 +0200

    Update complexities

--- prg1/datastructures.hh
+++ prg1/datastructures.hh
@@ -121,15 +121,15 @@ public:
     // Short rationale for estimate: The heaviest operation is a linear iteration through all map elements
     std::vector<StationID> all_stations();
 
-    // Estimate of performance: Average: O(1) Worst-case: O(n)
+    // Estimate of performance: Worst-case: O(n)
     // Short rationale for estimate: Average and worst-case complexities of unordered_map find() and insert().
     bool add_station(StationID id, Name const& name, Coord xy);
 
-    // Estimate of performance: Average: O(1) Worst-case: O(n)
+    // Estimate of performance: Worst-case: O(n)
     // Short rationale for estimate: Average and worst-case complexity of unordered_map find().
     Name get_station_name(StationID id);
 
-    // Estimate of performance: Average: O(1) Worst-case: O(n)
+    // Estimate of performance: Worst-case: O(n)
     // Short rationale for estimate: Average and worst-case complexity of unordered_map find().
     Coord get_station_coordinates(StationID id);
 
@@ -147,25 +147,25 @@ public:
     // Short rationale for estimate: Time complexity of find_if()
     StationID find_station_with_coord(Coord xy);
 
-    // Estimate of performance: Average: O(1), Worst-case: O(n)
+    // Estimate of performance: Worst-case: O(n)
     // Short rationale for estimate: Time complexity of unordered_map::find()
     bool change_station_coord(StationID id, Coord newcoord);
 
-    // Estimate of performance: Average: O(log(n)), Worst-case: O(n)
+    // Estimate of performance: Worst-case: O(n)
     // Short rationale for estimate: Average: map::at() logarithmic, worst-case: map::insert() linear
     bool add_departure(StationID stationid, TrainID trainid, Time time);
 
-    // Estimate of performance: Average: O(log(n)), worst-case: O(n)
+    // Estimate of performance: Worst-case: O(n)
     // Short rationale for estimate: Average: map::at() logarithmic, worst-case: map::erase() & map::find() linear
     bool remove_departure(StationID stationid, TrainID trainid, Time time);
 
-    // Estimate of performance: Average: O(n), worst-caseO(n^2)
+    // Estimate of performance: Worst-case: O(n^2)
     // Short rationale for estimate: Iterating through departure times and trains departing at those times, perftest seems to scale linearly
     std::vector<std::pair<Time, TrainID>> station_departures_after(StationID stationid, Time time);
 
     // We recommend you implement the operations below only after implementing the ones above
 
-    // Estimate of performance: Average: O(1), worst-case: O(n)
+    // Estimate of performance: Worst-case: O(n)
     // Short rationale for estimate: Average and worst-case time complexities of unordered_map::find() and ::insert()
     bool add_region(RegionID id, Name const& name, std::vector<Coord> coords);
 
@@ -173,41 +173,41 @@ public:
     // Short rationale for estimate: Iterating through an unordered map
     std::vector<RegionID> all_regions();
 
-    // Estimate of performance: Average O(1), worst: O(n)
-    // Short rationale for estimate: unordered_map::find()
+    // Estimate of performance: Worst-case: O(n)
+    // Short rationale for estimate: Time complexity of unordered_map::find()
     Name get_region_name(RegionID id);
 
-    // Estimate of performance: Average O(1), worst: O(n)
-    // Short rationale for estimate: unordered_map::find()
+    // Estimate of performance: Worst-case: O(n)
+    // Short rationale for estimate: Time complexity of unordered_map::find()
     std::vector<Coord> get_region_coords(RegionID id);
 
-    // Estimate of performance: Average O(1), worst: O(n)
-    // Short rationale for estimate: unordered_map::find()
+    // Estimate of performance: Worst-case: O(n)
+    // Short rationale for estimate: Time complexity of unordered_map::find()
     bool add_subregion_to_region(RegionID id, RegionID parentid);
 
-    // Estimate of performance: Average O(1), worst: O(n)
-    // Short rationale for estimate: unordered_map::find()
+    // Estimate of performance: Worst-case: O(n)
+    // Short rationale for estimate: Time complexity of unordered_map::find()
     bool add_station_to_region(StationID id, RegionID parentid);
 
-    // Estimate of performance: Average O(1), worst: O(n)
+    // Estimate of performance: Worst-case: O(n)
     // Short rationale for estimate: Recursive function getParents() calls unordered_map::find() and vector::push_back()
     std::vector<RegionID> station_in_regions(StationID id);
 
     // Non-compulsory operations
 
-    // Estimate of performance: Average: O(1), worst O(n^2)
+    // Estimate of performance: Worst-case: O(n^2)
     // Short rationale for estimate: Recursive unordered_map::find() calls
     std::vector<RegionID> all_subregions_of_region(RegionID id);
 
     // Estimate of performance: O(n*log(n))
-    // Short rationale for estimate: std::sort()
+    // Short rationale for estimate: Time complexity of std::sort()
     std::vector<StationID> stations_closest_to(Coord xy);
 
-    // Estimate of performance: Average O(1), worst: O(n)
+    // Estimate of performance: Worst-case: O(n)
     // Short rationale for estimate: unordered_map::find(), unordered_map::erase()
     bool remove_station(StationID id);
 
-    // Estimate of performance: Average: O(n), worst-case: O(n^2)
+    // Estimate of performance: Worst-case: O(n^2)
     // Short rationale for estimate: At most N1 * N2 comparisons (std::find_first_of), perftest seems to scale linearly
     RegionID common_parent_of_regions(RegionID id1, RegionID id2);
 
