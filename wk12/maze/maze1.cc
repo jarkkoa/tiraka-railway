4afd1990030d33ec4d0e4f2e8635ad30d8f46099
Jarkko Ahvenniemi <Jarge98@gmail.com>
2022-11-27 20:49:07 +0200

    Maze1 solution

--- wk12/maze/maze1.cc
+++ wk12/maze/maze1.cc
@@ -16,6 +16,24 @@ using namespace std;
  * @return Node*  A pointer to the node that is created. If the node already exists, return a pointer to the existing node
  */
 Node* createNode  (std::pair<int, int> loc, std::map<std::string, Node*> paths) {
-    std::cout << "createNode not implemented" << std::endl;
-    return NULL;
+
+
+    // Is the new node already a neighboring node's neighbor? (= already created)
+    for (const auto &node : paths)
+    {
+        for (const auto &neighbor : node.second->neighbors)
+        {
+            if (neighbor.second->location == loc)
+            {
+                return neighbor.second;
+            }
+        }
+    }
+
+    // Otherwise create a new node
+    Node* newNode = new Node;
+    newNode->location = loc;
+    newNode->neighbors = paths;
+
+    return newNode;
 }
